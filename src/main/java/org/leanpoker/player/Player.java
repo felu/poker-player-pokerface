1107502fdc71816cd7a0d510521f77944fee0975
Lukas Felber <lukas.felber@quatico.com>
2018-11-30 13:57:42 +0100

    Allways setting double smallblind.

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -7,7 +7,9 @@ public class Player {
     static final String VERSION = "Pokerface Java player";
 
     public static int betRequest(JsonElement request) {
-        return 2;
+        int smallblind = request.getAsJsonObject().get("small_blind").getAsInt();
+        System.out.println("Smallblind is: " + smallblind);
+        return 2 * smallblind;
     }
 
     public static void showdown(JsonElement game) {
