fb242c2a33dc9e7f2711d0ea7dd328225d5564f8
Lukas Felber <lukas.felber@quatico.com>
2018-11-30 14:26:37 +0100

    Removing game satee print

--- src/main/java/org/leanpoker/player/PlayerServlet.java
+++ src/main/java/org/leanpoker/player/PlayerServlet.java
@@ -21,7 +21,6 @@ public class PlayerServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if (req.getParameter("action").equals("bet_request")) {
             String gameState = req.getParameter("game_state");
-            System.out.println("GameState: " + gameState);
 
             resp.getWriter().print(Player.betRequest(new JsonParser().parse(gameState)));
         }
